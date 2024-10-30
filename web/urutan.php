3631924f6c03bbd4383424059b7c610c5c12b1f7
gatvinad <118316070+gatvinad@users.noreply.github.com>
2024-10-30 21:07:06 +0700

    Update urutan.php

--- web/urutan.php
+++ web/urutan.php
@@ -1,4 +1,8 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With");
+
 $a = $_GET['urutan'];
 if (!empty($a)){
     $fh = fopen("urutan.txt","w+");
