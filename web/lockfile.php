68cbb78eb02b610b285821423503e010885a27cc
gatvinad <118316070+gatvinad@users.noreply.github.com>
2024-10-31 11:03:05 +0700

    Update lockfile.php

--- web/lockfile.php
+++ web/lockfile.php
@@ -5,12 +5,18 @@ header("Access-Control-Allow-Headers: *");
 
 $a = $_GET['cek'];
 $b = $_GET['masuk'];
+$c = $_GET['reset'];
 
 if ($a == "yes"){
     echo file_get_contents('lockfile.txt');
 }else if ($b == "yes"){
     $fh = fopen("lockfile.txt","w+");
     fwrite($fh, 1);
+    fclose($fh);
+    echo 'done';
+}else if ($c == "yes"){
+    $fh = fopen("lockfile.txt","w+");
+    fwrite($fh, 0);
     fclose($fh);
     echo 'done';
 }else{
