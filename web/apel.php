2a9137554925de8e12e4d4601585e6d0511eb726
gatvinad <118316070+gatvinad@users.noreply.github.com>
2024-12-03 13:38:58 +0700

    Update apel.php

--- web/apel.php
+++ web/apel.php
@@ -7,128 +7,25 @@ $a = $_GET['nomor'];
 $b = $_GET['opsi'];
 $c = $_GET['apel'];
 
+// Pastikan input valid
+if (is_numeric($a) && is_numeric($b) && !empty($c)) {
+    $a = intval($a);
+    $b = intval($b);
 
-if (($a == '1') && $b == '1' && !empty($c)) {
-    $fh = fopen("./apel/1/1.txt","w+");
-    fwrite($fh, $c);
-    fclose($fh);
-    echo 'done';
-}else if (($a == '1') && $b == '2' && !empty($c)) {
-    $fh = fopen("./apel/1/2.txt","w+");
-    fwrite($fh, $c);
-    fclose($fh);
-    echo 'done';
-}else if (($a == '1') && $b == '3' && !empty($c)) {
-    $fh = fopen("./apel/1/3.txt","w+");
-    fwrite($fh, $c);
-    fclose($fh);
-    echo 'done';
-}else if (($a == '1') && $b == '4' && !empty($c)) {
-    $fh = fopen("./apel/1/4.txt","w+");
-    fwrite($fh, $c);
-    fclose($fh);
-    echo 'done';
-}else if (($a == '1') && $b == '5' && !empty($c)) {
-    $fh = fopen("./apel/1/5.txt","w+");
-    fwrite($fh, $c);
-    fclose($fh);
-    echo 'done';
-}else if (($a == '1') && $b == '6' && !empty($c)) {
-    $fh = fopen("./apel/1/6.txt","w+");
-    fwrite($fh, $c);
-    fclose($fh);
-    echo 'done';
-}else if (($a == '1') && $b == '7' && !empty($c)) {
-    $fh = fopen("./apel/1/7.txt","w+");
-    fwrite($fh, $c);
-    fclose($fh);
-    echo 'done';
-}else  if (($a == '1') && $b == '8' && !empty($c)) {
-    $fh = fopen("./apel/1/8.txt","w+");
-    fwrite($fh, $c);
-    fclose($fh);
-    echo 'done';
-}else if (($a == '2') && $b == '1' && !empty($c)) {
-    $fh = fopen("./apel/2/1.txt","w+");
-    fwrite($fh, $c);
-    fclose($fh);
-    echo 'done';
-}else if (($a == '2') && $b == '2' && !empty($c)) {
-    $fh = fopen("./apel/2/2.txt","w+");
-    fwrite($fh, $c);
-    fclose($fh);
-    echo 'done';
-}else if (($a == '2') && $b == '3' && !empty($c)) {
-    $fh = fopen("./apel/2/3.txt","w+");
-    fwrite($fh, $c);
-    fclose($fh);
-    echo 'done';
-}else if (($a == '2') && $b == '4' && !empty($c)) {
-    $fh = fopen("./apel/2/4.txt","w+");
-    fwrite($fh, $c);
-    fclose($fh);
-    echo 'done';
-}else if (($a == '2') && $b == '5' && !empty($c)) {
-    $fh = fopen("./apel/2/5.txt","w+");
-    fwrite($fh, $c);
-    fclose($fh);
-    echo 'done';
-}else if (($a == '2') && $b == '6' && !empty($c)) {
-    $fh = fopen("./apel/2/6.txt","w+");
-    fwrite($fh, $c);
-    fclose($fh);
-    echo 'done';
-}else if (($a == '2') && $b == '7' && !empty($c)) {
-    $fh = fopen("./apel/2/7.txt","w+");
-    fwrite($fh, $c);
-    fclose($fh);
-    echo 'done';
-}else  if (($a == '2') && $b == '8' && !empty($c)) {
-    $fh = fopen("./apel/2/8.txt","w+");
-    fwrite($fh, $c);
-    fclose($fh);
-    echo 'done';
-}else if (($a == '3') && $b == '1' && !empty($c)) {
-    $fh = fopen("./apel/3/1.txt","w+");
-    fwrite($fh, $c);
-    fclose($fh);
-    echo 'done';
-}else if (($a == '3') && $b == '2' && !empty($c)) {
-    $fh = fopen("./apel/3/2.txt","w+");
-    fwrite($fh, $c);
-    fclose($fh);
-    echo 'done';
-}else if (($a == '3') && $b == '3' && !empty($c)) {
-    $fh = fopen("./apel/3/3.txt","w+");
-    fwrite($fh, $c);
-    fclose($fh);
-    echo 'done';
-}else if (($a == '3') && $b == '4' && !empty($c)) {
-    $fh = fopen("./apel/3/4.txt","w+");
-    fwrite($fh, $c);
-    fclose($fh);
-    echo 'done';
-}else if (($a == '3') && $b == '5' && !empty($c)) {
-    $fh = fopen("./apel/3/5.txt","w+");
-    fwrite($fh, $c);
-    fclose($fh);
-    echo 'done';
-}else if (($a == '3') && $b == '6' && !empty($c)) {
-    $fh = fopen("./apel/3/6.txt","w+");
-    fwrite($fh, $c);
-    fclose($fh);
-    echo 'done';
-}else if (($a == '3') && $b == '7' && !empty($c)) {
-    $fh = fopen("./apel/3/7.txt","w+");
-    fwrite($fh, $c);
-    fclose($fh);
-    echo 'done';
-}else  if (($a == '3') && $b == '8' && !empty($c)) {
-    $fh = fopen("./apel/3/8.txt","w+");
-    fwrite($fh, $c);
-    fclose($fh);
-    echo 'done';
-}else{
+    // Periksa rentang yang valid untuk $a dan $b
+    if ($a >= 1 && $a <= 6 && $b >= 1 && $b <= 8) {
+        $filePath = "./apel/$a/$b.txt";
+
+        // Tulis ke file
+        if (file_put_contents($filePath, $c) !== false) {
+            echo 'done';
+        } else {
+            echo 'Error writing file';
+        }
+    } else {
+        echo 'Invalid range';
+    }
+} else {
     echo 'Restricted';
 }
 ?>
